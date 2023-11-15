4e0d8593a88efb6d78b08420e4fcf1418b2594a5
Eero Häihälä <eero.haihala@gmail.com>
2023-11-15 20:40:13 +0200

    Ignore warning logs from bevy_gltf::loader
    This speeds up startup time massively because that crate spams about how
    bevy doesn't implement interpolation for the type of keyframe blender
    export produces (step)

--- client/main/src/main.rs
+++ client/main/src/main.rs
@@ -1,12 +1,15 @@
 // use bevy::{diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}};
 // use bevy::ecs::schedule::ReportExecutionOrderAmbiguities;
-use bevy::prelude::*;
+use bevy::{log::LogPlugin, prelude::*};
 use whoops_all_grapplers_lib::WAGLib;
 
 fn main() {
     // Happens roughly in order, so add stages, click and assets before using them
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(LogPlugin {
+            filter: "wgpu=error,naga=warn,bevy_gltf::loader=error".to_string(),
+            ..default()
+        }))
         .add_plugins(WAGLib::default())
         // .add_plugin(LogDiagnosticsPlugin::default())
         // .add_plugin(FrameTimeDiagnosticsPlugin::default())

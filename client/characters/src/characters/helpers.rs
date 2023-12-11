98a1bc10661502ec0b305853f69ff58d7511860c
Eero Häihälä <eero.haihala@gmail.com>
2023-12-11 14:46:54 +0200

    Remove jump jitter

--- client/characters/src/characters/helpers.rs
+++ client/characters/src/characters/helpers.rs
@@ -10,13 +10,13 @@ pub fn jump(input: &'static str, impulse: Vec2, animation: impl Into<Animation>)
         vec![
             ActionBlock {
                 events: vec![animation.into().into()],
-                exit_requirement: ContinuationRequirement::Time(4),
+                exit_requirement: ContinuationRequirement::Time(3),
                 cancel_policy: CancelPolicy::never(),
                 mutator: None,
             },
             ActionBlock {
                 events: vec![Movement::impulse(impulse).into()],
-                exit_requirement: ContinuationRequirement::Time(6),
+                exit_requirement: ContinuationRequirement::Time(5),
                 cancel_policy: CancelPolicy::never(),
                 mutator: None,
             },

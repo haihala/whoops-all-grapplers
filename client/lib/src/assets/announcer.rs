197d4c3fe07e7131269c8fe9000066f36dc51311
Eero Häihälä <eero.haihala@gmail.com>
2024-12-24 14:52:01 +0200

    Make announcer clear existing voicelines when requesting a new one

--- client/lib/src/assets/announcer.rs
+++ client/lib/src/assets/announcer.rs
@@ -5,27 +5,33 @@ use bevy::prelude::*;
 #[derive(Debug, Resource, Default)]
 pub struct Announcer {
     stack: Vec<Sound>,
+    clear_existing: bool,
 }
 
 impl Announcer {
+    pub fn say(&mut self, stack: Vec<Sound>) {
+        self.stack = stack;
+        self.clear_existing = true;
+    }
+
     pub fn fight(&mut self) {
-        self.stack = vec![Sound::AnnouncerFight];
+        self.say(vec![Sound::AnnouncerFight]);
     }
 
     pub fn round_start(&mut self, round_number: usize) {
-        self.stack = vec![Sound::Number(round_number), Sound::AnnouncerRound];
+        self.say(vec![Sound::Number(round_number), Sound::AnnouncerRound]);
     }
 
     pub fn round_win(&mut self, winner: Player) {
-        self.stack = vec![
+        self.say(vec![
             Sound::AnnouncerWins,
             Sound::Number(winner.into()),
             Sound::AnnouncerPlayer,
-        ];
+        ]);
     }
 
     pub fn tie(&mut self) {
-        self.stack = vec![Sound::AnnouncerDraw];
+        self.say(vec![Sound::AnnouncerDraw]);
     }
 }
 
@@ -34,11 +40,18 @@ pub struct AnnouncerMarker;
 
 pub fn update_announcer(
     mut commands: Commands,
-    marked: Query<&AnnouncerMarker>,
+    marked: Query<Entity, With<AnnouncerMarker>>,
     mut announcer: ResMut<Announcer>,
 ) {
-    if !marked.is_empty() {
+    if !marked.is_empty() && !announcer.clear_existing {
         return;
+    }
+
+    if announcer.clear_existing {
+        for entity in &marked {
+            commands.entity(entity).despawn_recursive();
+        }
+        announcer.clear_existing = false;
     }
 
     let Some(next) = announcer.stack.pop() else {
